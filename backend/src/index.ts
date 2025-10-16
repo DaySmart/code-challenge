133efc782b4a7b8602cad70b1c9c497716ad4d1f
Jordan Rickman <jordan.rickman@daysmart.com>
2025-10-16 09:58:05 -0700

    Add CORS

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import taskRoutes from './routes/tasks';
 
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(express.json());
 
 // Routes
